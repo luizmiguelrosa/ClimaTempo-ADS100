7c41d6dc155e90b15c78af549586860186d3ff3b
Masso13 <luizmiguelrosalazarinirubio@gmail.com>
2023-06-02 15:18:45 -0300

    Tratando erro do usuário

--- server/main.py
+++ server/main.py
@@ -9,24 +9,27 @@ app = Flask(__name__)
 @app.route("/<cidade>")
 def clima(cidade):
     cidade = cidade.title()
-    if not "." in cidade:
-        clima = asyncio.run(pegarClima(cidade))
-        condicao = str(clima.current.kind).upper()
-        temperatura = converter_temperatura(clima.current.temperature)
-        
-        clima_seguinte = [forecast for forecast in clima.forecasts][1]
-        temperatura_seguinte = converter_temperatura(clima_seguinte.temperature)
+    if not "." in cidade and len(cidade) > 3:
+        try:
+            clima = asyncio.run(pegarClima(cidade))
+            condicao = str(clima.current.kind).upper()
+            temperatura = converter_temperatura(clima.current.temperature)
+            
+            clima_seguinte = [forecast for forecast in clima.forecasts][1]
+            temperatura_seguinte = converter_temperatura(clima_seguinte.temperature)
 
-        horario = datetime.now().strftime("%H:%M")
-        periodo = pegarPeriodo(horario)
-        
-        icone, fundo = pegarImagens(ESTADOS[condicao], periodo)
-        
-        icone_condicao = url_for('static', filename=f'img/{icone}')
-        fundo_horario = url_for('static', filename=f'img/{fundo}')
+            horario = datetime.now().strftime("%H:%M")
+            periodo = pegarPeriodo(horario)
+            
+            icone, fundo = pegarImagens(ESTADOS[condicao], periodo)
+            
+            icone_condicao = url_for('static', filename=f'img/{icone}')
+            fundo_horario = url_for('static', filename=f'img/{fundo}')
 
-        return render_template("index.html", cidade=cidade, condicao=ESTADOS[condicao], temperatura=temperatura, temperatura_seg=temperatura_seguinte, horario=horario, icone_condicao=icone_condicao, fundo=fundo_horario)
-    return abort(404)
+            return render_template("index.html", cidade=cidade, condicao=ESTADOS[condicao], temperatura=temperatura, temperatura_seg=temperatura_seguinte, horario=horario, icone_condicao=icone_condicao, fundo=fundo_horario)
+        except:
+            return render_template("error.html", erro="Aconteceu um erro interno no servidor")
+    return render_template("error.html", erro="Aconteceu um erro interno no servidor")
 
 if __name__ == "__main__":
     app.run(debug=True, host=sys.argv[1], port=sys.argv[2])
--- server/static/index.js
+++ server/static/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", (event)=>{
     let input_cidade = $("#cidade")[0]
-    if (event.key == "Enter") {
+    if (event.key == "Enter" && ! input_cidade.value.lenght > 3) {
         location.replace("http://192.168.1.2:8080/"+input_cidade.value)
     }
 })
